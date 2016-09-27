521439703f62d0cd7e6e8e2559a182a19c53169e
Ogrocki <ogrocki@ualberta.ca>
2016-09-26 20:54:39 -0400

    General changes, add some template for final recursive function

--- ot.java
+++ ot.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class ot
 {
 	public static class Waypoint {
@@ -13,13 +14,21 @@ public class ot
 			p=pp;
 		}
 		public void printWaypoint(){
+			//test function for quickly viewing waypoints
 			System.out.println("X:"+x);
 			System.out.println("Y:"+y);
 			System.out.println("P:"+p);
 		}
-		public double calcTravelTime(Waypoint destination){
-			return 2*Math.sqrt((destination.x-x)*(destination.x-x)+(destination.y-y)*(destination.y-y));
+		public double calcBestTime(Waypoint destination){
+			//calulation fuction for getting double value from two locations or the penalty if thats less
+			double travelTime= (0.5*Math.sqrt((destination.x-x)*(destination.x-x)+(destination.y-y)*(destination.y-y)))+10;
+			if(travelTime>destination.p) return destination.p;
+			else return travelTime;
 		}
+		public double calcTravelTime(Waypoint destination){
+			return (0.5*Math.sqrt((destination.x-x)*(destination.x-x)+(destination.y-y)*(destination.y-y)))+10;
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -29,18 +38,25 @@ public class ot
         int robotx=0, roboty=0;
         while(i!=0){ 
         	ArrayList<Waypoint> list = new ArrayList<Waypoint>();
+        	Waypoint Waypoint0 =new Waypoint(0,0,1000000);
+        	list.add(Waypoint0);
         	for(int ii=0; ii<i; ii++){ //runs for set amount of test cases
         		x= scan.nextInt();
         		y= scan.nextInt();
         		p= scan.nextInt();
         		Waypoint Waypoint =new Waypoint(x,y,p);
         		Waypoint.printWaypoint();
-        		list.add(Waypoint);
-        		
+        		list.add(Waypoint);   		
         	}
-        	//TODO: ADD CALULATION OF BEST DISTANCES
-        	
+        	Waypoint Waypoint00 =new Waypoint(100,100,1000000);
+        	list.add(Waypoint00);
+        	//System.out.printf("%.3f", Waypoint0.calcBestTime(list.get(0))+list.get(0).calcBestTime(Waypoint1));
+        	System.out.println(minTravelTime(0,list,0));
         	i=scan.nextInt();
         }
+    }
+    public static double minTravelTime(double total, ArrayList<Waypoint> list, int counter){
+    	//TODO THIS SYSTEM
+    	return 0.0;
     }
 }
